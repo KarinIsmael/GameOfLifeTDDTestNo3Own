9899f42d23ec071e4b544f3d9e54a6a8d08d6163
Karin Ismael <karin.ismael@hotmail.com>
2022-01-15 16:03:12 +0100

    Changes by arif

--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -5,14 +5,19 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class GameOfLifeTest {
 
-GameOfLife gameOfLife = new GameOfLife(4,8);
+    private final GameOfLife gameOfLife;
+
+    public GameOfLifeTest(){
+        gameOfLife = new GameOfLife(4,8);
+    }
 
 
 @Test
 public void OneAloneCellHasNoNeighbours(){
-    int numberofNeighbours = gameOfLife.numberOfAliveNeighbours(1, 2);
 
-    assertThat(numberofNeighbours).isEqualTo(0);
+    int numberOfNeighbours = gameOfLife.numberOfAliveNeighbours(1, 2);
+
+    assertThat(numberOfNeighbours).isEqualTo(0);
 
 }
 }
