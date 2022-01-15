bd430f80fff95f0cbf191b7c678e492c44124e80
Karin Ismael <karin.ismael@hotmail.com>
2022-01-15 18:32:21 +0100

    CheckThatGameFindsSixNeighbors - Changes by arif

--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -79,4 +79,19 @@ public void OneAloneCellHasNoNeighbours(){
 
         assertThat(neighboursCount).isEqualTo(5);
     }
+
+    @Test
+    public void CheckThatGameFindsSixNeighbors() {
+
+        gameOfLife.cellIsAlive(1, 3);
+        gameOfLife.cellIsAlive(2, 3);
+        gameOfLife.cellIsAlive(1,1);
+        gameOfLife.cellIsAlive(2,1);
+        gameOfLife.cellIsAlive(1,2);
+        gameOfLife.cellIsAlive(3,3);
+
+        int neighboursCount = gameOfLife.numberOfAliveNeighbours(2, 2);
+
+        assertThat(neighboursCount).isEqualTo(6);
+    }
 }
